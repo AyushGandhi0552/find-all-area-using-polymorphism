b85a5122ef81134839a1ef03bc360f10301fd2f2
ayush <ayush@AYUSHGANDHI>
2024-02-17 17:45:41 +0530

    Add project files.

---
+++ find all area using polymorphism/Calculation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace find_all_area_using_polymorphism
+{
+    public class Calculation
+    {
+        public void area(int l) 
+        {
+            Console.WriteLine("The area of circle is:"+l*l);
+        }
+
+        public void area(int l,int w)
+        {
+            Console.WriteLine("The area of rectangle is:"+l*w);
+        }
+
+        public void area(float r)
+        {
+            float pi = 3.14f;
+            Console.WriteLine(pi*r*r);
+        }
+    }
+}
---
+++ find all area using polymorphism/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace find_all_area_using_polymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculation calculation = new Calculation();
+
+            calculation.area(2, 2);
+            calculation.area(4);
+            calculation.area(3f);
+            Console.ReadKey();
+        }
+    }
+}
